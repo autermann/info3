553978035895d6cf18225a3040bda4bd7c511d2b
AndreaWietz <43341301+AndreaWietz@users.noreply.github.com>
2021-01-25 19:07:27 +0100

    Delete auktionen directory

--- auktionen/AuctionElement.java
+++
@@ -1,7 +0,0 @@
-package auktionen;
-
-public interface AuctionElement {
-    void accept(Visitor v);
-    String getName();
-    void setName(String name);
-}
--- auktionen/Visitor.java
+++
@@ -1,6 +0,0 @@
-package auktionen;
-
-public interface Visitor {
-    void visit(Category category);
-    void visit(Article auction);
-}
